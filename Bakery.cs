3a8feb1206ad70cc6d7646daa7148a9659f90c11
Aaron Whang <tlbhibiki@gmail.com>
2019-07-19 12:44:25 -0700

    add error handling. It will catch non int and negative numbers

--- Bakery.cs
+++ Bakery.cs
@@ -10,16 +10,50 @@ namespace Patisserie
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("How many loaves of bread would you like? Each loaf is $5 each. We also have a buy 2 get 1 free deal.");
-      int BreadInput = int.Parse(Console.ReadLine());
-      Console.WriteLine("How many Pastries would you like? Pastries are $2 each or $5 for 3.");
-      int PastryInput = int.Parse(Console.ReadLine());
+      Console.Write("How many Loaves of Bread would you like? Loaves are $5 each. ");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.WriteLine("We also have a buy 2 get 1 free deal.");
+      Console.ResetColor();
+      bool numCheck = false;
+      string BreadInput = Console.ReadLine();
+      int BreadNum = 0;
+      while (numCheck == false)
+      {
+        if (int.TryParse(BreadInput, out BreadNum) && BreadNum >= 0)
+        {
+          numCheck = true;
+        } else
+        {
+          Console.WriteLine("Please enter a valid number.");
+          BreadInput = Console.ReadLine();
+        }
+      }
 
-      Bread bread = new Bread(BreadInput);
-      Pastry pastry = new Pastry(PastryInput);
+      Console.Write("How many Pastries would you like? Pastries are $2 each or ");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.WriteLine("$5 for 3.");
+      Console.ResetColor();
+      string PastryInput = Console.ReadLine();
+      int PastryNum = 0;
+      numCheck = false;
+      while (numCheck == false)
+      {
+        if (int.TryParse(PastryInput, out PastryNum) && PastryNum >= 0)
+        {
+          numCheck = true;
+        } else
+        {
+          Console.WriteLine("Please enter a valid number.");
+          PastryInput = Console.ReadLine();
+        }
+      }
+
+      Bread bread = new Bread(BreadNum);
+      Pastry pastry = new Pastry(PastryNum);
       int TotalPrice = bread.BreadTotal + pastry.PastryTotal;
 
-      Console.ForegroundColor = ConsoleColor.Red;
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("__________________________________________________");
       Console.WriteLine("Your Bread comes out to $" + bread.BreadTotal);
       Console.WriteLine("Your Pastries comes out to $" + pastry.PastryTotal);
       Console.WriteLine("You owe $" + TotalPrice);
